0260a2ac4d91c82c5c9c34792754fd2117c66025
leilani5u <a_barrientos.m@seg.guanajuato.gob.mx>
2026-06-04 12:22:00 -0600

    Delete src directory

--- src/config/business.ts
+++
@@ -1,7 +0,0 @@
-﻿export const restaurantConfig = {
-  name: "Mariscos Gutierrez",
-  whatsappPhone: "",
-  pickupLine: "Pasare a recoger mi pedido.",
-  fallbackNotice:
-    "Aun no hay numero oficial. Te abrimos WhatsApp con el pedido listo para compartir."
-};
--- src/lib/format.ts
+++
@@ -1,6 +0,0 @@
-export const formatCurrency = (value: number) =>
-  new Intl.NumberFormat("es-MX", {
-    style: "currency",
-    currency: "MXN",
-    maximumFractionDigits: 0
-  }).format(value);
